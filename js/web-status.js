1af717ffef505660fd03119a553122cefd7958c9
ShanRenYi - CTB <88585893+SRY-CTB@users.noreply.github.com>
2024-01-01 01:09:11 +0800

    Add files via upload

---
+++ js/web-status.js
@@ -0,0 +1,40 @@
+const idsToCheck = [795811475, 796066690, 796066693, 796066687];
+const url = 'https://api.star-skin.cn/timebot/status.php';
+
+async function getStatusAndUpdateHTML() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const statuses = idsToCheck.map(id => data.monitors.find(monitor => monitor.id === id)?.status);
+
+    updateStatus('Normal', statuses.every(status => status === 'normal'));
+
+    function updateStatus(statusText, isNormal) {
+      const statusElements1 = document.querySelectorAll('.status-right');
+      statusElements1.forEach(element => {
+        const textElement = element.firstChild;
+        textElement.textContent = statusText + ' ';
+
+        if (statusText === 'Normal') {
+          element.querySelector('.bx-signal-3').classList.remove('bx-flip-horizontal');
+        } else {
+          element.querySelector('.bx-signal-3').classList.add('bx-flip-horizontal');
+        }
+      });
+
+      const statusElements2 = document.querySelectorAll('.status-right-2');
+      statusElements2.forEach(element => {
+        if (isNormal) {
+          element.textContent = 'Normal';
+        } else {
+          element.textContent = 'Abnormal';
+        }
+      });
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+getStatusAndUpdateHTML();
